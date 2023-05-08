c7118ce13363954e58eff867c6fab1a991c96639
ture403 <ture403@gmail.com>
2023-05-08 13:00:23 +0900

    2023-05-08

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+// function clock(){
+//   let clock = document.getElementById("clock");
+//   setInterval(function(){
+//     clock.innerHTML  = new Date().toLocaleDateString();
+//   },1000)
+// }
+
+// function clock(){
+//   const element = (
+//     <div>
+//       <div>hello, webstroyboy</div>
+//       <h2>지금은 {new Date().toLocaleDateString()}입니다.</h2>
+//     </div>
+//   );
+
+//   ReactDOM.render(element, document.getElementById("root"));
+// }
+// export default clock
+
+function App(){
+  return (
+    <div>
+      <h1>리엑트</h1>
+    </div>
+  )
+}
+export default App;
